57da98c874a514f5e72991ab03d1baf2386e9658
Kenneth Burroughs <kjbur04@gmail.com>
2026-02-23 13:10:28 -0600

    Update Driver.cpp

--- Driver.cpp
+++ Driver.cpp
@@ -1,34 +1,28 @@
 #ifndef _STACK_H
 #define _STACK_H
 #include <iostream>
-// Note: `Stack.h` should declare the `Stack` class and constants like `MAX`.
-// This driver expects that header to be available in the include path.
 using namespace std;
 
 int main() {
 
-	// Create a stack object (initially empty)
+	// Initialize stack
 	Stack stack;
 
-	// Push two values onto the stack and announce each operation
+	// Push two elements
 	stack.Push(1);
-	std::cout << "Pushed 1" << std::endl;
 	stack.Push(2);
-	std::cout << "Pushed 2" << std::endl;
 
-	// Print the contents of the stack (top -> bottom)
+	// Print stack elements
 	stack.Print();
 
-	// Pop a value into `x` and print the popped value if successful
+	// Pop a value into "x"
 	int x;
 	if (stack.Pop(x)) {
 		std::cout << "Popped " << x << std::endl;
 	}
 
-	// Print the stack contents again after the pop
+	// Print after popping
 	stack.Print();
 
 	return 0;
 }
-
-#endif
